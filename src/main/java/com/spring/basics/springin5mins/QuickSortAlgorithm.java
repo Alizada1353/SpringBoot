7ccdaf186c408c1e02eb1bf97cc29896679b2267
Alizada1353 <abdulrahim.alizada@powercoders.org>
2022-12-19 15:02:10 +0100

    commented debug mode, set quickSort as primary Algo

--- src/main/java/com/spring/basics/springin5mins/QuickSortAlgorithm.java
+++ src/main/java/com/spring/basics/springin5mins/QuickSortAlgorithm.java
@@ -1,5 +1,10 @@
 package com.spring.basics.springin5mins;
 
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary
 public class QuickSortAlgorithm implements SortAlgorithm {
     public int[] sort(int[] numbers) {
 
